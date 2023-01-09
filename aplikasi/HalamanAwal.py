c1067ee27ad15861053ca9c15cf7c9b4eb53a878
DhimasWeray <dhimweray222@gmail.com>
2023-01-09 09:20:09 +0700

    cba tag

--- aplikasi/HalamanAwal.py
+++ aplikasi/HalamanAwal.py
@@ -5,11 +5,11 @@ from  PIL import Image, ImageEnhance
 st.title('Deteksi Masker')
 
 
-uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+uploaded_file = st.file_uploader("", type=['jpg','png','jpeg','gift'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     col1, col2, col3 = st.columns(3)
 
     with col2:
         st.markdown('<p style="text-align: center;">Gambar</p>',unsafe_allow_html=True)
-        st.image(image,width=300)  
+        st.image(image,width=300)
